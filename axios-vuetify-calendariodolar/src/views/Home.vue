5572de8b904856984e52d28358b0128452a77b48
evesotomayor <evesotomayorstar@gmail.com>
2022-02-10 18:21:22 -0300

    actualizar

--- axios-vuetify-calendariodolar/src/views/Home.vue
+++ axios-vuetify-calendariodolar/src/views/Home.vue
@@ -1,15 +1,76 @@
 <template>
-  <hello-world />
+  <div>
+    <v-layout :wrap="true">
+      <v-flex xs12>
+        <v-card>
+          <v-date-picker
+            v-model="fecha"
+            full-width
+            locale="es-cl"
+            :min="minimo"
+            :max="maximo"
+            @change="getDolar(fecha)"
+          >
+          </v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-center">
+            {{ valor }}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import axios from "axios";
+import { mapMutations } from "vuex";
 
-  export default {
-    name: 'Home',
+export default {
+  name: "Home",
+  data() {
+    return {
+      fecha: new Date().toISOString().substr(0, 10),
+      minimo: "1984",
+      maximo: new Date().toISOString().substr(0, 10),
+      valor: null,
+    };
+  },
+  methods: {
+    ...mapMutations(['mostrarLoading', 'ocultarLoading']),    
 
-    components: {
-      HelloWorld,
+    async getDolar(dia) {
+      // console.log(dia);
+      let arrayFecha = dia.split(["-"])
+      // console.log(arrayFecha);
+      let ddmmyy = arrayFecha[2] + "-" + arrayFecha[1] + "-" + arrayFecha[0];
+      // console.log(ddmmyy);
+
+      try {
+        
+        this.mostrarLoading({titulo: 'Accediendo a información', color: 'secondary'})
+
+        let datos = await axios.get(
+          `https://www.mindicador.cl/api/dolar/${ddmmyy}`
+        )
+
+        if (datos.data.serie.length > 0) {
+          this.valor = await datos.data.serie[0].valor;
+
+        } else {
+          this.valor = 'Sin resultados'
+        }
+        // console.log(datos.data);
+      } catch (error) {
+        // console.log(error);
+      } finally {
+        this.ocultarLoading()
+      }
     },
-  }
+  },
+  created() {
+    this.getDolar(this.fecha);
+  },
+};
 </script>
